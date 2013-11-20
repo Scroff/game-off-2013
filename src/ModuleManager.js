d51db98923a8daf9dda42cbb673e2e10ba67d9ca
Kieran Doonan <kieran.doonan@cloudreach.co.uk>
2013-11-20 14:22:10 +0000

    Created ModuleManager.js but it isn't finished or implemented

---
+++ src/ModuleManager.js
@@ -0,0 +1,55 @@
+var ModuleManager = (function () {
+  // Constructor
+  function ModuleManager(stage, world) {
+    this._modules = [];
+    this._moduleDefs = [];
+    this._createQueue = []; // Queue of moduleDef sprites that need modules created.
+    this._moduleSpriteUnderMouse = null;
+    this._mouseSprite = null;
+    this._mouse = {'x': 0, 'y': 0};
+    this._mouseOffset = {'x': 0, 'y': 0};
+
+    this._stage = stage;
+    this._world = world;
+  };
+
+  ModuleManager.prototype.addModuleDef = function (
+    type, 
+    bitmapData, 
+    width, 
+    height) {
+    var moduleDef = new ModuleDef(type, bitmapData, width, height, this._world),
+        sprite = moduleDef.getSprite();
+    sprite.index = this._moduleDefs.length;
+    sprite.addEventListener(MouseEvent.MOUSE_UP, this._moduleDefSpriteMouseUp);
+    sprite.addEventListener(MouseEvent.MOUSE_OVER, this._moduleSpriteUnderMouse);
+    sprite.visible = false; // Don't show until it's under mouse
+    this._stage.addChild(sprite);
+    this._moduleDefs.push(moduleDef);
+  };
+
+  // Called every frame to update all modules
+  // mouse = position of the mouse with x and y values
+  ModuleManager.prototype.update = function (mouse) {
+    for(module in modules) {
+      module.update();
+    }
+  };
+
+  // Events //
+  // To be added to module def sprite and called when mouse is released
+  ModuleManager.prototype._moduleDefSpriteMouseUp = function (e) {
+    if(this._mouseSprite = e.currentTarget) {
+      var module = this._moduleDefs[e.currentTarget.index].spawnModule(mouse.x, mouse.y)
+      this._mouseSprite.visible = false; // TODO: HERE
+    };
+  };
+
+  // To be added to module def sprites and called when mouse is over them
+  // Keeps a track of what is under the mouse for when we want to attach things
+  ModuleManager.prototype._mouseOverModuleSprite = function (e) {
+    this._moduleSpriteUnderMouse = e.currentTarget;
+  };
+  
+  return ModuleManager;
+};
